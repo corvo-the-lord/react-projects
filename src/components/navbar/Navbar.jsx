b75b3ab5ee32ae2ab83d7fb1ebedf6d048e2ec8c
Andres Calderon <andrexcr19@gmail.com>
2023-05-17 01:10:34 -0600

    assets agregados, implementacion de navbar con respectivas hojas de estilo, Update de client-rendering API's, Cambio de ReactDom.render a Create Root por implementacion de React 18, hoja de estilo index agregada

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
-import './navbar.css';
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+import React, { useState } from "react";
+import { RiMenu3Line, RiCloseLin, RiCloseLine } from "react-icons/ri";
+import "./navbar.css";
+import logo from "../../assets/logo.svg";
+//BEM -> Block Elememt Modifier
 
-export default Navbar
+const Menu = () => (
+    <>
+        <p>
+            <a href="#home">Home</a>
+        </p>
+        <p>
+            <a href="#wgpt3">What is GPT?</a>
+        </p>
+        <p>
+            <a href="#possibility">Open AI</a>
+        </p>
+        <p>
+            <a href="#features">Case studies</a>
+        </p>
+        <p>
+            <a href="#blog">Library</a>
+        </p>
+    </>
+);
+const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
+
+    return (
+        <div className="gpt3__navbar">
+            <div className="gpt3__navbar-links">
+                <div className="gpt3__navbar-links_logo">
+                    <img src={logo} alt="logo" />
+                </div>
+                <div className="gpt3__navbar-links_container">
+                    <Menu />
+                </div>
+            </div>
+            <div className="gpt3__navbar-sign">
+                <p>Sing in</p>
+                <button type="button">Sing up</button>
+            </div>
+            <div className="gpt3__navbar-menu">
+                {toggleMenu ? (
+                    <RiCloseLine
+                        color="#fff"
+                        size={27}
+                        onClick={() => setToggleMenu(false)}
+                    />
+                ) : (
+                    <RiMenu3Line
+                        color="#fff"
+                        size={27}
+                        onClick={() => setToggleMenu(true)}
+                    />
+                )}
+                {toggleMenu && (
+                    <div className="gpt3__navbar-menu_container scale-up-center">
+                        <div className="gpt3__navbar-menu_container-links">
+                            <Menu />
+                            <div className="gpt3__navbar-menu_container-links-sign">
+                                <p>Sing in</p>
+                                <button type="button">Sing up</button>
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
